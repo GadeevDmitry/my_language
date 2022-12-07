8db1f4aca74b9111f6ebfb7c35fef3a7668de166
GadeevDmitry <gadeev.dima@mail.ru>
2022-12-07 14:50:02 +0300

    Refactore labels

---
+++ cpu/src/label.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+
+#include "../../lib/logs/log.h"
+#include "../../lib/read_write/read_write.h"
+#include "../../lib/algorithm/algorithm.h"
+#include "../../lib/stack/stack.h"
+
+#include "label.h"
+
+/*===========================================================================================================================*/
+// LABEL_STORE_CTOR_DTOR
+/*===========================================================================================================================*/
+
+void label_store_ctor(label_store *const link)
+{
+    assert(link != nullptr);
+
+    link->store    = nullptr;
+    link->size     = 0;
+    link->capacity = 0;
+}
+
+bool label_store_ctor(label_store *const link, const int capacity)
+{
+    assert(link     != nullptr);
+    assert(capacity >=       0);
+
+    link->label    = (label *) log_calloc(capacity, sizeof(label));
+    link->capacity = capacity;
+    link->size     = 0;
+    
+    if (link->label == nullptr)
+    {
+        log_error(        "can't allocate memory for label array(%d)\n", __LINE__);
+        fprintf  (stderr, "can't allocate memory for label array\n");
+        return false;
+    }
+    return true;
+}
+
+void label_store_dtor(label_store *const link)
+{
+    assert(link != nullptr);
+
+    log_free(link->label);
+}
+
+/*===========================================================================================================================*/
+// LABEL_STORE_PUSH
+/*===========================================================================================================================*/
+
+void label_store_push(label_store *const link, const int token_num, const int label_pc)
+{
+    assert(link != nullptr);
+    assert(link->size < link->capacity);
+
+    link->store[link->size].token_num = token_num;
+    link->store[link->size].pc        = label_pc;
+
+    link->size++;
+}
